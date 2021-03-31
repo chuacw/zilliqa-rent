73899ddf5a886919272f25516947ff3cbb046c07
unknown <hakngrow@gmail.com>
2021-03-31 13:24:43 +0800

    31032021 1324

--- controllers/ListingsController.js
+++ controllers/ListingsController.js
@@ -10,15 +10,87 @@ router.use(function timeLog (req, res, next) {
 })
 
 router.get('/', getListings)
+router.get('/add/:address/:rent', addListing)
+router.get('/rent/:id/:amount', rent)
+
+router.get('/state', getState)
+
+router.get('/add', addHouse)
+router.post('/new', newHouse)
 
 async function getListings(req, res, next) {
 
     console.log('LISTINGS GET: ALL')
 
-    svcListings.getAll()
-        .then(listings => res.json(listings))
-        .catch(err => res.json({message: err}))
+    svcListings.getAll().then(listings => {
+
+        res.render('listings', {
+            title: 'Listings of Houses for Rent',
+            houses: listings 
+        })
+    })
 }
 
+
+async function addListing(req, res, next) {
+
+    let address = req.params.address
+    let rent = req.params.rent
+
+    console.log(`LISTINGS ADD: ${address} ${rent}`)
+
+    svcListings.addHouse(address, rent).then(result => {
+
+        console.log(result)
+    })
+}
+
+
+async function rent(req, res, next) {
+
+    let id = req.params.id
+    let amount = req.params.amount
+
+    console.log(`LISTINGS RENT: ${id} ${amount}`)
+
+    svcListings.rentHouse(id, amount).then(result => {
+
+        console.log(result)
+    })
+}
+
+async function addHouse(req, res, next) {
+
+    console.log('LISTINGS ADD_HOUSE:')
+
+        res.render('add', {
+            title: 'Add House'
+        })
+}
+
+
+async function newHouse(req, res, next) {
+
+    let address = req.body.txtAddress
+    let rent = req.body.txtRent
+
+    console.log(`LISTINGS NEW_HOUSE: ${address} ${rent}`)
+
+    svcListings.addHouse(address, rent).then(result => {
+
+        console.log(result)
+    })
+}
+
+
+async function getState(req, res, next) {
+
+    console.log('LISTINGS GET_STATE:')
+
+    svcListings.getState().then(result => {
+
+        console.log(result)
+    })
+}
 
 module.exports = router
--- service/ListingsService.js
+++ service/ListingsService.js
@@ -33,6 +33,7 @@ const contractAirBnB = zilliqa.contracts.at(
 )
 
 async function initMyGasPrice () {
+
   try {
     // Get Balance
     const balance = await zilliqa.blockchain.getBalance(address)
@@ -88,15 +89,185 @@ async function getAll () {
     console.log(confirmedTxn.receipt)
 
     if (confirmedTxn.receipt.success === true) {
-      console.log(confirmedTxn.receipt.transitions[0].msg.params)
+
+        let parameters = confirmedTxn.receipt.transitions[0].msg.params
+
+        code = parameters[0]
+        arrAddresses = parameters[1].value
+        arrRents = parameters[2].value
+        arrStatuses = parameters[3].value
+
+        console.log(arrAddresses)
+
+        let arrHouses = []
+
+        arrAddresses.forEach(objAddr => {
+            
+            let houseId = objAddr.arguments[0]
+            let address = objAddr.arguments[1]
+
+            let rent = arrRents.find(objRent => objRent.arguments[0] === houseId).arguments[1]
+            let status = arrStatuses.find(objStatus => objStatus.arguments[0] === houseId).arguments[1]
+
+            let house = {
+                "id": houseId,
+                "address": address, 
+                "rent": rent,
+                "status": status
+            }
+
+            console.log(JSON.stringify(house))
+
+            arrHouses.push(house)
+        });
+
+        return arrHouses
     }
-  } catch (err) {
+  } 
+  catch (err) {
     console.log(err)
   }
+}
+
+
+async function addHouse (address, rent) {
+
+    try {
+
+        const callTx = await contractAirBnB.callWithoutConfirm(
+          'addHouse',
+          [
+            {
+                vname: 'address',
+                type: 'String',
+                value: address,
+            },
+            {
+                vname: 'rent',
+                type: 'Uint128',
+                value: rent,
+            }
+          ],
+          {
+            // amount, gasPrice and gasLimit must be explicitly provided
+            version: VERSION,
+            amount: new BN(0),
+            gasPrice: myGasPrice,
+            gasLimit: Long.fromNumber(8000)
+          },
+          false
+        )
+        
+        console.log(callTx.bytes)
+        // check the pending status
+        const pendingStatus = await zilliqa.blockchain.getPendingTxn(callTx.id)
+        console.log(`Pending status is: `)
+        console.log(pendingStatus)
+
+        // process confirm
+        console.log(`The transaction id is:`, callTx.id)
+        console.log(`Waiting transaction be confirmed`)
+        const confirmedTxn = await callTx.confirm(callTx.id)
+
+        console.log(`The transaction status is:`)
+        console.log(confirmedTxn.receipt)
+
+        if (confirmedTxn.receipt.success === true) {
+            return 'OK'
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+
+async function rentHouse (id, amount) {
+
+    try {
+
+        const callTx = await contractAirBnB.callWithoutConfirm(
+          'rentHouse',
+          [
+            {
+                vname: 'houseId',
+                type: 'Uint32',
+                value: id,
+            }
+          ],
+          {
+            // amount, gasPrice and gasLimit must be explicitly provided
+            version: VERSION,
+            amount: new BN(amount),
+            gasPrice: myGasPrice,
+            gasLimit: Long.fromNumber(8000)
+          },
+          false
+        )
+        
+        console.log(callTx.bytes)
+        // check the pending status
+        const pendingStatus = await zilliqa.blockchain.getPendingTxn(callTx.id)
+        console.log(`Pending status is: `)
+        console.log(pendingStatus)
+
+        // process confirm
+        console.log(`The transaction id is:`, callTx.id)
+        console.log(`Waiting transaction be confirmed`)
+        const confirmedTxn = await callTx.confirm(callTx.id)
+
+        console.log(`The transaction status is:`)
+        console.log(confirmedTxn.receipt)
+
+        if (confirmedTxn.receipt.success === true) {
+            return 'OK'
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+async function getState () {
+
+    try {
+
+        const contractState = await contractAirBnB.getState()
+
+        let objAddresses = contractState.houseAddresses
+        let objRents = contractState.houseRents
+        let objStatuses = contractState.houseStatus
+
+        let ids = Object.keys(objAddresses)
+
+        let arrHouses = []
+
+        ids.forEach(id => {
+
+            let address = objAddresses[id]
+            let rent = objRents[id]
+            let status = objStatuses[id]
+
+            let house = {
+                "id": id,
+                "address": address,
+                "rent": rent,
+                "status": status
+            }
+
+            arrHouses.push(house)
+        })
+
+        console.log(arrHouses)
+
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 initMyGasPrice()
 
 module.exports = {
-  getAll
+  getAll, addHouse, rentHouse,getState
 }
